a9384b8812fa47ed2ffb445adebc0fb7bddffd52
chayi548 <167800726+Chay548@users.noreply.github.com>
2024-10-18 20:45:33 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-public class  Hello
-{
-    public static void main(String a[] args)
-    {
+public class Hello {
+    public static void main(String[] args) {
         System.out.println("helloworlddd");
     }
 }
